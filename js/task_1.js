4b41123991e2530fdee910fa0c87e695912f07c2
Liudmila Popescu <liudmila.popescu91@gmail.com>
2020-12-21 12:59:42 +0000

    task 1 done

---
+++ js/task_1.js
@@ -0,0 +1,16 @@
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+const userKeys = Object.keys(user);
+
+for (const key of userKeys) {
+    console.log(key + ':'+ user[key]);
+}
